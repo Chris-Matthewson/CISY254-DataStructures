36657007595714a09c12569df866d876a5934c86
Chris Matthewson <cmatthewson2133@gmail.com>
2015-09-22 17:53:34 -0400

    Added structure to StringBag
    -Created all method signatures and fields
    -No logic yet

---
+++ src/Lab3_CollectionClass/StringBag.java
@@ -0,0 +1,82 @@
+package Lab3_CollectionClass;
+
+/**
+ * Created by chrismatthewson on 9/22/15.
+ */
+public class StringBag
+{
+    /*  FIELDS  */
+    private String[] data; //the data being stored
+    private int numElements; //the number of elements in the actual array
+
+
+
+    /* CONSTRUCTORS */
+    public StringBag()
+    {
+        //not implemented
+    }
+
+    public StringBag(int initCapacity)
+    {
+        //not implemented
+    }
+
+
+
+    /*  PRIVATE METHODS */
+    private void enlargeArray()
+    {
+        //not implemented
+    }
+
+
+
+    /*  ACCESSORS   */
+    public int getCapacity()
+    {
+        //not implemented
+    }
+
+    public int getSize()
+    {
+        //not implemented
+    }
+
+    public boolean exists(String taget)
+    {
+        //not implemented
+    }
+
+    public int countOccurence(String target)
+    {
+        //not implemented
+    }
+
+
+
+
+    /* MUTATORS */
+    public void add(String newElement)
+    {
+        //not implemented
+    }
+
+    public void add(String[] newElements)
+    {
+        //not implemented
+    }
+
+    public void remove(String target)
+    {
+        //not implemented
+    }
+
+
+
+    /*  OVERRIDES   */
+    public String toString()
+    {
+        //not implemented
+    }
+}
